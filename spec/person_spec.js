ba9e4430758402b8bc79e11fd1314e27455c40e1
lebogang-cloud <mphagol@gmail.com>
2020-01-15 15:08:58 +0200

    updated

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,9 +1,41 @@
-let Person = require("../src/person")
-let person = new Person('Ryan',30,'male',['being a hardarse', ' agile', ' ssd hard drives']);
+let Person = require("../src/person");
+let person = new Person("Ryan", 30, "male", [
+  "being a hardarse",
+  " agile",
+  " ssd hard drives"
+]);
 
-describe ('the generic data and functionality of a human', function() {
-    it("should contain collection of attributes and functions", function(){
-        expect(person.hello()).toEqual('Hello, my name is Ryan and I am 30 years old male. My interests are being a hardarse, agile, ssd hard drives.')
-    })
+describe("the generic data and functionality of a human", function() {
+  it("should contain collection of attributes and functions", function() {
+    expect(person.hello()).toEqual(
+      "Hello, my name is Ryan and I am 30 years old male. My interests are being a hardarse, agile, ssd hard drives."
+    );
+  });
+
+  it("should contain a name", function() {
+    let ryan = new Person("Ryan", 30, "male", [
+      "being a hardarse",
+      " agile",
+      " ssd hard drives"
+    ]);
+    expect(ryan.name).toEqual("Ryan");
+  });
+
+  it("should contain a name", function() {
+    let ryan = new Person("Ryan", 30, "male", [
+      "being a hardarse",
+      " agile",
+      " ssd hard drives"
+    ]);
+    expect(ryan.age).toEqual(30);
+  });
+
+  it("should contain a name", function() {
+    let ryan = new Person("Ryan", 30, "male", [
+      "being a hardarse",
+      " agile",
+      " ssd hard drives"
+    ]);
+    expect(ryan.gender).toEqual("male");
+  });
 });
-    
--- src/person.js
+++ src/person.js
@@ -1,21 +1,38 @@
 class Person {
     constructor(name, age, gender, interest) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.interest = interest;
-
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+      this.interest = interest;
     }
-    hello(){
-        return "Hello, my name is" + " " + this.name  + " " + "and I am" + " " + this.age + " " + "years old " + this.gender + "." + " My interests are " + this.interest + ".";
-
+    hello() {
+      return (
+        "Hello, my name is" +
+        " " +
+        this.name +
+        " " +
+        "and I am" +
+        " " +
+        this.age +
+        " " +
+        "years old " +
+        this.gender +
+        "." +
+        " My interests are " +
+        this.interest +
+        "."
+      );
     }
-}
-
-
-let person = new Person('Ryan',30,'male',['being a hardarse', ' agile', ' ssd hard drives']);
-let greeting = person.hello()
-console.log(greeting)
-
-
-module.exports = Person;
+  }
+  
+  let person = new Person("Ryan", 30, "male", [
+    "being a hardarse",
+    " agile",
+    " ssd hard drives"
+  ]);
+  
+  let greeting = person.hello();
+  console.log(greeting);
+  
+  module.exports = Person;
+  
